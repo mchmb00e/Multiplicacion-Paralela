37e1993827beeae57a1523f7c2acb944240e1db9
Miguel Chamorro <miguelchamorro912@gmail.com>
2025-09-28 16:47:02 -0300

    add. sequential

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Multiplicación Paralela
 ## Compilación de prueba unitaria temporal
 
-    python3 test.py m n k
+    python3 test.py m n k -M -O
 
 $m:\text{Cantidad de dígitos del primero número}$
 $n:\text{Cantidad de dígitos del segundo número}$
--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define VERBOSE_MODE 'V'
 #define SILENCE_MODE 'S'
 #define TRADITIONAL_METHOD 'T'
 #define RUSSIAN_METHOD 'R'
 
+int array_to_int(int n, int *A) {
+    int number = 0;
+    for (int i = 0; i < n; i++) {
+        number = number * 10 + A[i];
+    }
+    return number;
+}
+
+
 int main(int argc, char *argv[]) {
 
     // Declaración de variables
-    int m, n, k;
+    int size_A, size_B, k;
     int *A, *B;
     char method, mode;
 
@@ -32,20 +42,20 @@ int main(int argc, char *argv[]) {
 
 
     // Lectura de cantidad de digitos del num. 1
-    scanf("%d", &m);
-    printf("Tamaño del primer número  = %d | Número: ", m);
-    A = calloc(m, sizeof(int));
-    for (int i = 0; i < m; i++) {
+    scanf("%d", &size_A);
+    printf("Tamaño del primer número  = %d | Número: ", size_A);
+    A = calloc(size_A, sizeof(int));
+    for (int i = 0; i < size_A; i++) {
         scanf("%d", &A[i]);
         printf("%d", A[i]);
     }
     printf("\n");
 
     // Lectura de cantidad de digitos del num. 2
-    scanf("%d", &n);
-    printf("Tamaño del segundo número = %d | Número: ", n);
-    B = calloc(n, sizeof(int));
-    for (int i = 0; i < n; i++) {
+    scanf("%d", &size_B);
+    printf("Tamaño del segundo número = %d | Número: ", size_B);
+    B = calloc(size_B, sizeof(int));
+    for (int i = 0; i < size_B; i++) {
         scanf("%d", &B[i]);
         printf("%d", B[i]);
     }
@@ -54,8 +64,21 @@ int main(int argc, char *argv[]) {
 
     if (k == 1) {
         printf("\n\nImplementación secuencial.\n\n");
+        if (method == TRADITIONAL_METHOD) {
+            int pivot, sum = 0, *C;
+            C = calloc(size_B, sizeof(int));
+            pivot = array_to_int(size_A, A);
+            for (int i = 0; i < size_B; i++) {
+                C[i] = B[i] * pivot;
+            }
+            for (int i = 0; i < size_B; i++) {
+                printf("sum = sum + %d * 10^%d\n", C[i], size_B-(i+1));
+                sum = sum + C[i] * pow(10, size_B-(i+1));
+            }
+            printf("Resultado: %d\n", sum);
+        } else if (method == RUSSIAN_METHOD) {
 
-        
+        }
 
     } else {
         printf("\n\nImplementación paralela.\n\n");
